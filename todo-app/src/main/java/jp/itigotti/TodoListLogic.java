3cc44c0d725eb9a1887d1189888a4cfc59ddb397
itigo0802 <itigo0802@gmail.com>
2026-01-30 13:50:42 +0900

    各入力が空白時の例外処理の実装

--- todo-app/src/main/java/jp/itigotti/TodoListLogic.java
+++ todo-app/src/main/java/jp/itigotti/TodoListLogic.java
@@ -12,9 +12,15 @@ public class TodoListLogic {
 	}
 	
 	public void addTodoItem(String task, LocalDate expirationDate) {
-		if(task != null && !task.isBlank() && expirationDate != null) {
-			todoItems.add(new TodoItemModel(task, expirationDate));
+		if(task == null || task.isBlank()) {
+			throw new IllegalArgumentException("タスクが入力されていません");
 		}
+
+		if(expirationDate == null) {
+			throw new IllegalArgumentException("期限が入力されていません");
+		}
+
+		todoItems.add(new TodoItemModel(task, expirationDate));
 	}
 	
 	public void removeTodoItem(TodoItemModel item) {
