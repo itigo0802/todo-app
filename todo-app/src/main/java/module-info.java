3a5509dad44ded35c586797e5573dfc4457e2139
itigo0802 <itigo0802@gmail.com>
2026-01-31 22:59:19 +0900

    module-infoを修正

--- todo-app/src/main/java/module-info.java
+++ todo-app/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module jp.itigotti {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
     requires java.sql;
 
     opens jp.itigotti to javafx.fxml;
